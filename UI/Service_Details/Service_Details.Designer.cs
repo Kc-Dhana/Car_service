59278c4986e96d8518ad390f067ff58d98a92f26
Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-02-22 12:09:13 +0530

    Add_service_detail_ui

---
+++ UI/Service_Details/Service_Details.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace A__Auto_Care
+{
+    partial class Service_Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_back = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_clear = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_search_all = new Guna.UI2.WinForms.Guna2Button();
+            this.dataGridView_service = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_sno = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btn_search = new Guna.UI2.WinForms.Guna2Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl_err_pno = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_service)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_back
+            // 
+            this.btn_back.AutoRoundedCorners = true;
+            this.btn_back.BackColor = System.Drawing.Color.Transparent;
+            this.btn_back.BorderRadius = 21;
+            this.btn_back.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_back.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_back.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_back.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_back.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btn_back.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_back.ForeColor = System.Drawing.Color.White;
+            this.btn_back.HoverState.FillColor = System.Drawing.Color.White;
+            this.btn_back.HoverState.ForeColor = System.Drawing.Color.Red;
+            this.btn_back.Location = new System.Drawing.Point(923, 673);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(110, 45);
+            this.btn_back.TabIndex = 39;
+            this.btn_back.Text = "Back";
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.AutoRoundedCorners = true;
+            this.btn_clear.BackColor = System.Drawing.Color.Transparent;
+            this.btn_clear.BorderRadius = 21;
+            this.btn_clear.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_clear.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_clear.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_clear.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_clear.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btn_clear.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.ForeColor = System.Drawing.Color.White;
+            this.btn_clear.HoverState.FillColor = System.Drawing.Color.White;
+            this.btn_clear.HoverState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btn_clear.Location = new System.Drawing.Point(1039, 673);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(110, 45);
+            this.btn_clear.TabIndex = 38;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_search_all
+            // 
+            this.btn_search_all.AutoRoundedCorners = true;
+            this.btn_search_all.BackColor = System.Drawing.Color.Transparent;
+            this.btn_search_all.BorderRadius = 21;
+            this.btn_search_all.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_search_all.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_search_all.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_search_all.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_search_all.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btn_search_all.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_search_all.ForeColor = System.Drawing.Color.White;
+            this.btn_search_all.HoverState.FillColor = System.Drawing.Color.White;
+            this.btn_search_all.HoverState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btn_search_all.Location = new System.Drawing.Point(910, 121);
+            this.btn_search_all.Name = "btn_search_all";
+            this.btn_search_all.Size = new System.Drawing.Size(180, 45);
+            this.btn_search_all.TabIndex = 37;
+            this.btn_search_all.Text = "Search All";
+            this.btn_search_all.Click += new System.EventHandler(this.btn_search_all_Click);
+            // 
+            // dataGridView_service
+            // 
+            this.dataGridView_service.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView_service.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_service.Location = new System.Drawing.Point(35, 209);
+            this.dataGridView_service.Name = "dataGridView_service";
+            this.dataGridView_service.RowHeadersWidth = 51;
+            this.dataGridView_service.RowTemplate.Height = 24;
+            this.dataGridView_service.Size = new System.Drawing.Size(1147, 430);
+            this.dataGridView_service.TabIndex = 36;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(147, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Service No.";
+            // 
+            // txt_sno
+            // 
+            this.txt_sno.AutoRoundedCorners = true;
+            this.txt_sno.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.txt_sno.BorderRadius = 21;
+            this.txt_sno.BorderThickness = 2;
+            this.txt_sno.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_sno.DefaultText = "";
+            this.txt_sno.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txt_sno.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txt_sno.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_sno.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txt_sno.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_sno.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_sno.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sno.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt_sno.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txt_sno.Location = new System.Drawing.Point(303, 121);
+            this.txt_sno.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_sno.Name = "txt_sno";
+            this.txt_sno.PasswordChar = '\0';
+            this.txt_sno.PlaceholderText = "";
+            this.txt_sno.SelectedText = "";
+            this.txt_sno.Size = new System.Drawing.Size(386, 45);
+            this.txt_sno.TabIndex = 34;
+            // 
+            // btn_search
+            // 
+            this.btn_search.AutoRoundedCorners = true;
+            this.btn_search.BackColor = System.Drawing.Color.Transparent;
+            this.btn_search.BorderRadius = 21;
+            this.btn_search.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_search.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_search.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_search.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_search.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btn_search.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_search.ForeColor = System.Drawing.Color.White;
+            this.btn_search.HoverState.FillColor = System.Drawing.Color.White;
+            this.btn_search.HoverState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btn_search.Location = new System.Drawing.Point(724, 121);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(180, 45);
+            this.btn_search.TabIndex = 33;
+            this.btn_search.Text = "Search";
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label8.Location = new System.Drawing.Point(69, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(209, 38);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "Service Details";
+            // 
+            // lbl_err_pno
+            // 
+            this.lbl_err_pno.AutoSize = true;
+            this.lbl_err_pno.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_err_pno.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_err_pno.Location = new System.Drawing.Point(312, 170);
+            this.lbl_err_pno.Name = "lbl_err_pno";
+            this.lbl_err_pno.Size = new System.Drawing.Size(0, 20);
+            this.lbl_err_pno.TabIndex = 47;
+            // 
+            // Service_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1216, 751);
+            this.Controls.Add(this.lbl_err_pno);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_search_all);
+            this.Controls.Add(this.dataGridView_service);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_sno);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.label8);
+            this.Name = "Service_Details";
+            this.Text = "Service Details";
+            this.Load += new System.EventHandler(this.Service_Details_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_service)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btn_back;
+        private Guna.UI2.WinForms.Guna2Button btn_clear;
+        private Guna.UI2.WinForms.Guna2Button btn_search_all;
+        private System.Windows.Forms.DataGridView dataGridView_service;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox txt_sno;
+        private Guna.UI2.WinForms.Guna2Button btn_search;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_err_pno;
+    }
+}
