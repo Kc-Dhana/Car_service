4a2e7367999b5378453ad604cc3e4c29e22d8c15
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-03-12 22:01:06 +0530

    Add_Check_qty_ui

---
+++ UI/Check_Qty/Check_Qty.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace A__Auto_Care
+{
+    public partial class Check_Qty : Form
+    {
+        public Check_Qty()
+        {
+            InitializeComponent();
+        }
+        DBConection obj = new DBConection();
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            dataGridView_parts.DataSource = null;
+        }
+
+        private void btn_check_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "Select * from Part where Qty<10";
+                dataGridView_parts.DataSource = obj.Display(query);
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Data cannot Show", "Error",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(" Check Again", "Error",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
